3b7dccc8556b58d65ba4cdac725c9d4aad41c24f
DerHexer17 <hendrik.thues@gmail.com>
2015-03-16 18:11:44 +0100

    Weitere Spielstatistiken ausgebaut

--- src/com/example/hvs/SpielWeiteresFragment.java
+++ src/com/example/hvs/SpielWeiteresFragment.java
@@ -2,6 +2,7 @@ package com.example.hvs;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,25 +38,31 @@ public class SpielWeiteresFragment extends Fragment {
 		gastteamName.setText(spiel.getTeamGast());
 		
 		TextView heimteamToreImSchnitt = (TextView) rootView.findViewById(R.id.spielWeiteresHeimteamToreImSchnitt);
-		heimteamToreImSchnitt.setText(durchschnittlicheHeimtore());
+		heimteamToreImSchnitt.setText(durchschnittlicheHeimtore(spiel.getTeamHeim()));
 		
 		TextView gastteamToreImSchnitt = (TextView) rootView.findViewById(R.id.spielWeiteresGastteamToreImSchnitt);
-		gastteamToreImSchnitt.setText(durchschnittlicheGasttore());
+		gastteamToreImSchnitt.setText(durchschnittlicheGasttore(spiel.getTeamGast()));
 		
+		TextView heimteamHeimsieg = (TextView) rootView.findViewById(R.id.spielWeiteresHeimteamSieg);
+		heimteamHeimsieg.setText(hoechsterHeimsieg(spiel.getTeamHeim()));
+		
+		TextView gastteamSieg = (TextView) rootView.findViewById(R.id.spielWeiteresGastteamSieg);
+		gastteamSieg.setText(hoechsterAuswaertssieg(spiel.getTeamGast()));
 		
 
 		return rootView;
 	}
 	
-	public String durchschnittlicheHeimtore(){
+	public String durchschnittlicheHeimtore(String heimteam){
 		double anzahlGespielt = 0;
 		double anzahlHeimtore = 0;
 		for (Spiel s : dbh.getAllTeamGames(ligaNr, spiel.getTeamHeim())) {
 			
 			
-			if (s.getToreHeim() > 0) {
+			if (s.getToreHeim() > 0 && s.getTeamHeim().equals(heimteam)) {
 				anzahlHeimtore += s.getToreHeim();
 				anzahlGespielt++;
+				Log.d("Statistik", s.getTeamHeim()+s.getTeamGast()+s.getToreHeim()+s.getToreGast());
 			}
 
 		}
@@ -63,17 +70,17 @@ public class SpielWeiteresFragment extends Fragment {
 
 		
 		return String.format("%.4g%n", result);
-		
+				
 		//return Double.toString(result);
 	}
 	
-	public String durchschnittlicheGasttore(){
+	public String durchschnittlicheGasttore(String gastteam){
 		double anzahlGespielt = 0;
 		double anzahlGasttore = 0;
 		for (Spiel s : dbh.getAllTeamGames(ligaNr, spiel.getTeamGast())) {
 			
 			
-			if (s.getToreGast() > 0) {
+			if (s.getToreGast() > 0 && s.getTeamGast().equals(gastteam)) {
 				anzahlGasttore += s.getToreGast();
 				anzahlGespielt++;
 			}
@@ -85,5 +92,35 @@ public class SpielWeiteresFragment extends Fragment {
 		return String.format("%.4g%n", result);
 		
 		//return Double.toString(result);
+	}
+	
+	public String hoechsterHeimsieg(String heimteam){
+		int maxDifferenz = 0;
+		int differenz = 0;
+		Spiel heimsieg = null;
+		for(Spiel s : dbh.getAllTeamGames(ligaNr, spiel.getTeamHeim())){
+			differenz = s.getToreHeim()-s.getToreGast();
+			if(differenz > maxDifferenz && s.getTeamHeim().equals(heimteam)){
+				heimsieg = s;
+				maxDifferenz = differenz;
+			}
+		}
+		
+		return "- gegen "+heimsieg.getTeamGast()+" ("+heimsieg.getToreHeim()+":"+heimsieg.getToreGast()+")";
+	}
+	
+	public String hoechsterAuswaertssieg(String gastteam){
+		int maxDifferenz = 0;
+		int differenz = 0;
+		Spiel gastsieg = null;
+		for(Spiel s : dbh.getAllTeamGames(ligaNr, spiel.getTeamGast())){
+			differenz = s.getToreGast()-s.getToreHeim();
+			if(differenz > maxDifferenz && s.getTeamGast().equals(gastteam)){
+				gastsieg = s;
+				maxDifferenz = differenz;
+			}
+		}
+		
+		return "- bei "+gastsieg.getTeamHeim()+" ("+gastsieg.getToreHeim()+":"+gastsieg.getToreGast()+")";
 	}
 }
